f7e12965f8d67c8c51a507effdb827070078e35b
Marcelo Melo <marcelovmelo201@gmail.com>
2024-11-02 18:52:40 -0300

    commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,29 @@
+package application;
+
+import entities.StringManipulator;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Iniciando o Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Solicitando dados ao usuário
+        System.out.print("Digite a frase: ");
+
+        //Armazenando os dados em uma variável do tipo String
+        String phrase = sc.nextLine();
+
+        //Armazenando o número de vogais em uma varívael utilizando função da classe String manipulator
+        int vowels = StringManipulator.countVowels(phrase);
+
+        //Imprimindo o resultado na tela
+        System.out.println("Número de vogais na frase: "+ vowels);
+
+        sc.close();
+
+
     }
 }
--- src/entities/StringManipulator.java
+++ src/entities/StringManipulator.java
@@ -1,2 +1,33 @@
-package entities;public class StringManipulator {
+package entities;
+
+import java.util.Locale;
+
+public class StringManipulator {
+
+    public static int countVowels(String phrase){
+
+        //Inicializando variável contador
+        int count = 0;
+
+        //Transformando a variável phrase totalmente escrita em minúsculo
+        String _phrase = phrase.toLowerCase(Locale.ROOT);
+
+        //Varrendo toda a String utilizando um for
+        for(int i = 0; i < _phrase.length(); i++){
+
+            //Selecionando cada letra e armazenando em uma variável Char
+            char letter = _phrase.charAt(i);
+
+            //Verificando se a letra é vogal
+            if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'){
+
+                //Contando o número de vogais
+                count ++;
+            }
+        }
+
+        //Retornando variável contador
+        return count;
+    }
+
 }
